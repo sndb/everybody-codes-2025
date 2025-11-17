92bd2f7102fb78ab92380b83c56b819840494f75
sndb <ckyg@proton.me>
2025-11-17 15:20:03 +0300

    day 3

---
+++ day03/part3.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"os"
+	"sort"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	crates := []int{}
+	data, _ := io.ReadAll(os.Stdin)
+	for s := range strings.SplitSeq(strings.TrimSpace(string(data)), ",") {
+		n, _ := strconv.Atoi(s)
+		crates = append(crates, n)
+	}
+
+	sort.Ints(crates)
+
+	var run, sets int
+	for i, c := range crates {
+		if i > 0 && c == crates[i-1] {
+			run++
+			sets = max(sets, run)
+		} else {
+			run = 1
+		}
+	}
+	fmt.Println(sets)
+}
